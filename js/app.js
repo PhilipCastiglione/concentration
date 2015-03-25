b37df7ca627979fc5dae737995ccb3050e7b3dcd
Philip Castiglione <philipcastiglione@gmail.com>
2015-03-25 18:59:29 +1100

    reset cards on round end and rework guessRound vs checkSlot

--- js/app.js
+++ js/app.js
@@ -18,24 +18,26 @@ var app = {
     },
     guessesInRound: 0,
     guessesMade: 0,
-    guessRound: function() {
-      function checkProgress(){
-        if (app.game.guessesMade === app.game.guessesInRound) {
-          clearInterval(app.game.gameTimer);
-          console.log("finally motherfucker");
-        }
+    checkProgress: function() {
+      if (app.game.guessesMade === app.game.guessesInRound) {
+        clearInterval(app.game.gameTimer);
+        console.log("guessRound end motherfucker");
       }
+    },
+    guessRound: function() {
       app.cards.firstCard = "";
+      app.slots.openSlot1 = "";
+      app.slots.openSlot2 = "";
+      app.slots.openSlot3 = "";
       app.game.guessesInRound = 1;
       app.game.guessesMade = 0;
-      app.game.gameTimer = setInterval(checkProgress, 1000);
-
+      app.game.gameTimer = setInterval(app.game.checkProgress, 1000);
     }
   },
 
   slots: {
     initSlots: function() {
-      for (var i=0;i<16;i++){
+      for (var i = 0; i < 16; i++) {
         app.slots[i] = {
           card: "",
           matches: 0
@@ -48,17 +50,26 @@ var app = {
         return;
       }
       app.game.guessesMade++;
-      var clickedCard = app.slots[event.target.id.slice(4)].card;
+      var clickedSlot = event.target.id.slice(4);
+      var clickedCard = app.slots[clickedSlot].card;
+      app.slots['openSlot'+app.game.guessesMade] = clickedSlot;
       app.slots.viewSlot(clickedCard);
       app.cards.compareCards(clickedCard);
       if (app.game.guessesInRound === 1) {
-        app.game.guessesInRound = app.slots[event.target.id.slice(4)].matches;
+        app.game.guessesInRound = app.slots[clickedSlot].matches;
         app.cards.firstCard = clickedCard;
       }
     },
     viewSlot: function(card) {
       $(event.target).attr('src', card);
-    }
+    },
+    resetSlot: function(slot) {
+      $('#slot' + slot).attr('src', 'images/blue.jpg');
+      // set timeout
+    },
+    openSlot1: '',
+    openSlot2: '',
+    openSlot3: ''
   },
 
   cards: {
@@ -85,13 +96,18 @@ var app = {
         app.slots[i].matches = slotCards[i][1];
       }
     },
-    compareCards: function(card){
+    compareCards: function(card) {
       if (app.cards.firstCard && app.cards.firstCard !== card) {
-        // do a round end loss thingo
-        console.log("nawww that's shit u lose");
+        for (var i = 1; i <= app.game.guessesMade; i++) {
+          var slotToReset = app.slots['openSlot'+i];
+          app.slots.resetSlot(slotToReset);
+        }
       }
     },
     firstCard: ""
   }
-
 }
+
+// make view temporary (based on difficulty)
+// win condition
+// 2player lol
